bb4d0cc3fd929d52eb7e73be3f675137af4a55f5
david <hongdingzhu@hotmail.com>
2019-02-18 23:34:04 -0800

    Check in new solution

---
+++ SMLYS.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SMLYS.Web.Data;
+
+[assembly: HostingStartup(typeof(SMLYS.Web.Areas.Identity.IdentityHostingStartup))]
+namespace SMLYS.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
