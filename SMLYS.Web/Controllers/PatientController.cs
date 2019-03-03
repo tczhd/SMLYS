ff8cb66fd2d08661f38c80d5c3232576c6b09c7b
david <hongdingzhu@hotmail.com>
2019-03-02 16:15:53 -0800

    Fix version issue

--- SMLYS.Web/Controllers/PatientController.cs
+++ SMLYS.Web/Controllers/PatientController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SMLYS.Web.Controllers
 {
+    [Authorize]
     public class PatientController : Controller
     {
         // GET: /<controller>/
