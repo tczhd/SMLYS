12395630fb25c54f8c7c6c64dccbad44ea73838a
david <hongdingzhu@hotmail.com>
2019-03-15 00:00:14 -0700

    Check in code

--- SMLYS.Web/wwwroot/js/views/Patients/newPatient.js
+++ SMLYS.Web/wwwroot/js/views/Patients/newPatient.js
@@ -1,9 +1,46 @@
 ﻿//var SMLYS = {};
+$(document).ready(function () {
+
+    var myForm = $('#myForm');
+    var patientBody = myForm.find('div.patient-body');
+
+    var patient_base_form = $('#patient_base_form').html();
+    var patient_base_form_address = $('#patient_base_form_address').html();
+    var patient_base_form_action = $('#patient_base_form_action').html();
+
+    patientBody.append($(patient_base_form).clone());
+    patientBody.append($(patient_base_form_address).clone());
+    patientBody.append($(patient_base_form_action));
+
+    $('#add_family_member').click(function () {
+        var cardHeader = '<div class="card-header"><strong>Add family member / Dependent</strong></div>'
+        var checkBoxAddess = '<div class="card-block"><input type="checkbox" name="AddressCheckBox"  checked="checked" /> Address same as primary member  </div >';
+        var cardBody = $('<div class="card-body patient-body"> </div>');
+        cardBody.append($(patient_base_form).clone());
+        cardBody.append($(patient_base_form_address).clone());
+
+        $(cardHeader).insertBefore(".add-patient-buttons");
+        $(checkBoxAddess).insertBefore(".add-patient-buttons");
+        cardBody.insertBefore(".add-patient-buttons");
+    });
+});
+
 
 SMLYS.Patient = {
 
     AddNewPatientModal: function () {
+
+        //var isvalid = $("#myForm").valid();  // Tells whether the form is valid
+
+        //if (isvalid) {
+        //    var a = 1;
+        //}
+        //else {
+        //    var b = 2;
+        //}
+
         var spinner = "<img src='/images/loading.gif' width='30' height='30' ></img>";
+        $('#primaryModal').modal('show');
 
         var dataType = 'application/json; charset=utf-8';
         var modalBody = $('div.modal-body');
